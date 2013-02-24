986db63f7317c040f8dcefe2f2f7654adb60ba80
Yannick Méheut <useless@utouch.fr>
2013-02-24 19:28:30 +0100

    indentation problem in some files

--- parrot_server/MainServer.py
+++ parrot_server/MainServer.py
@@ -5,70 +5,70 @@ import os, socket
 import ParrotServer
 
 class MainServer():
-    def __init__(self, port):
-	'''This function is the MainServer class constructor:
-	* port: the port to listen to'''
-	# Default values, you can change them
-	MainServer.default_words_frequences = [('vite', 0.8), ('BOUM', 1.0)]
-	MainServer.max_parrot_servers = 10
+	def __init__(self, port):
+		'''This function is the MainServer class constructor:
+		* port: the port to listen to'''
+		# Default values, you can change them
+		MainServer.default_words_frequences = [('vite', 0.8), ('BOUM', 1.0)]
+		MainServer.max_parrot_servers = 10
 
-	# We make sure the configuration is sound
-	assert(len(MainServer.default_words_frequences) <= MainServer.max_parrot_servers)
+		# We make sure the configuration is sound
+		assert(len(MainServer.default_words_frequences) <= MainServer.max_parrot_servers)
 
-	self.port = port
+		self.port = port
 
-	# We initialize the socket, and tell the OS
-	# we want to reuse the address
-	self.socket = socket.socket()
-	self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	self.socket.bind(('', self.port))
-	self.socket.listen(5)
+		# We initialize the socket, and tell the OS
+		# we want to reuse the address
+		self.socket = socket.socket()
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind(('', self.port))
+		self.socket.listen(5)
 
-	# We initialize the MainServer's arguments
-	self.parrot_servers = []
-	self.child_pids = []
-	self.client_sockets = []
+		# We initialize the MainServer's arguments
+		self.parrot_servers = []
+		self.child_pids = []
+		self.client_sockets = []
 
-    def add_parrot_server(self, word, frequence):
-	'''This function adds a ParrotServer to the list of ParrotServers:
-	* word: the word the ParrotServer should send
-	* frequence: the frequence the ParrotServer should wait'''
-	# We create a new ParrotServer, and append it to the list
-	new_parrot_server = ParrotServer.ParrotServer(self, word, frequence)
-	self.parrot_servers.append(new_parrot_server)
+	def add_parrot_server(self, word, frequence):
+		'''This function adds a ParrotServer to the list of ParrotServers:
+		* word: the word the ParrotServer should send
+		* frequence: the frequence the ParrotServer should wait'''
+		# We create a new ParrotServer, and append it to the list
+		new_parrot_server = ParrotServer.ParrotServer(self, word, frequence)
+		self.parrot_servers.append(new_parrot_server)
 
-	# We fork, if in the child process, we start the ParrotServer.
-	# If not, we add the PID to the list
-	child_pid = os.fork()
-	if child_pid:
-	    self.child_pids.append(child_pid)
-	else:
-	    new_parrot_server.send_word()
+		# We fork, if in the child process, we start the ParrotServer.
+		# If not, we add the PID to the list
+		child_pid = os.fork()
+		if child_pid:
+			self.child_pids.append(child_pid)
+		else:
+			new_parrot_server.send_word()
 
-    def launch(self):
-	'''This function launches the MainServer. It will first
-	add the default ParrotServers to the MainServer, then it
-	will wait for client connections'''
-	print 'Listening on %d' % self.port
+	def launch(self):
+		'''This function launches the MainServer. It will first
+		add the default ParrotServers to the MainServer, then it
+		will wait for client connections'''
+		print 'Listening on %d' % self.port
 
-	# For every (word, frequence) couples, we
-	# add a ParrotServer
-	for (word, frequence) in MainServer.default_words_frequences:
-	    self.add_parrot_server(word, frequence)
+		# For every (word, frequence) couples, we
+		# add a ParrotServer
+		for (word, frequence) in MainServer.default_words_frequences:
+			self.add_parrot_server(word, frequence)
 
-	try:
-	    while True:
-		# We wait for a client to connect
-		client_socket, (client_ip, client_port) = self.socket.accept()
-		print 'New client from %s:%d' % (client_ip, client_port)
+		try:
+			while True:
+				# We wait for a client to connect
+				client_socket, (client_ip, client_port) = self.socket.accept()
+				print 'New client from %s:%d' % (client_ip, client_port)
 
-		# When he does, we add it to the other clients
-		self.client_sockets.append(client_socket)
-		self.client_sockets[-1].send('Connected to the parrot server. Have fun!\n')
-	except KeyboardInterrupt:
-	    # When interrupted, we kill every child process,
-	    # we close the socket and exit
-	    for child_pid in self.child_pids:
-		os.kill(child_pid, 9)
-	    self.socket.close()
+				# When he does, we add it to the other clients
+				self.client_sockets.append(client_socket)
+				self.client_sockets[-1].send('Connected to the parrot server. Have fun!\n')
+		except KeyboardInterrupt:
+			# When interrupted, we kill every child process,
+			# we close the socket and exit
+			for child_pid in self.child_pids:
+				os.kill(child_pid, 9)
+			self.socket.close()
 
--- parrot_server/ParrotServer.py
+++ parrot_server/ParrotServer.py
@@ -6,39 +6,36 @@ import time, random
 
 class ParrotServer():
     def __init__(self, parent, word, frequence):
-	'''This function is the constructor for the ParrotServer class:
-	    * parent: the calling MainServer
-	    * word: the word we\'ll send
-	    * frequence: the frequence we\'ll wait'''
-	# Default parameter, change it to desired value
-	ParrotServer.min_frequence = 0.5
+		'''This function is the constructor for the ParrotServer class:
+			* parent: the calling MainServer
+			* word: the word we\'ll send
+			* frequence: the frequence we\'ll wait'''
+		# Default parameter, change it to desired value
+		ParrotServer.min_frequence = 0.5
 
-	# We initialize the parameters
-	self.parent = parent
-	self.word = word
-	# If the frequence is correct, we keep it
-	if frequence >= ParrotServer.min_frequence:
-	    self.frequence = frequence
-	# If it's negative, we take a random smaller frequence
-	elif frequence <= -1.0 * ParrotServer.min_frequence:
-	    self.frequence = -1.0*ParrotServer.min_frequence*random.random()
+		# We initialize the parameters
+		self.parent = parent
+		self.word = word
+		# If the frequence is correct, we keep it
+		if frequence >= ParrotServer.min_frequence:
+			self.frequence = frequence
+		# If it's negative, we take a random smaller frequence
+		elif frequence <= -1.0 * ParrotServer.min_frequence:
+			self.frequence = -1.0*ParrotServer.min_frequence*random.random()
 	# FIXME we have to handle the case where the frequence is not correct
 
-	print '[PID: %5i] New parrot server: (%s, %f)' % (os.getpid(), self.word, self.frequence)
-
     def send_word(self):
-	'''This function sends the specified word at
-	the specified frequence'''
-	try:
-	    print '[PID: %5i] Sending %s' % (os.getpid(), self.word)
-	    while True:
-		# We send the word to every client
-		for client_socket in self.parent.client_sockets:
-		    print '[PID: %5i] client: %s' % (os.getpid(), client_socket)
-		    client_socket.send(self.word)
-		# And we sleep the desired frequence
-		time.sleep(self.frequence)
-	except KeyboardInterrupt:
-	    # When killed, we exit
-	    return	
+		'''This function sends the specified word at
+		the specified frequence'''
+		try:
+			print '[PID: %5i] ParrotServer, sending %s' % (os.getpid(), self.word)
+			while True:
+				# We send the word to every client
+				for client_socket in self.parent.client_sockets:
+					client_socket.send(self.word)
+			# And we sleep the desired frequence
+			time.sleep(self.frequence)
+		except KeyboardInterrupt:
+			# When killed, we exit
+			return	
 
